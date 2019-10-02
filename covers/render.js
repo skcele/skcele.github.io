6dd1c6b5a5bdf78b546b713501a935e4900de3c0
Stephanie Cree <wingedwords36@gmail.com>
2019-10-01 22:27:03 -0500

    Fixed cover and responsive bugs

--- covers/render.js
+++ covers/render.js
@@ -1,29 +1,26 @@
-/*
-var animData = {
-        container: document.getElementById('gatsbycontainer'),
-        renderer: 'svg',
-        loop: true,
-        autoplay: true,
-        path: 'gamin.json'
-    };
-    var anim = bodymovin.loadAnimation(animData);;
-*/
+document.addEventListener('readystatechange', function() {
+    if (event.target.readyState !== 'complete') {
+        return
+    }
 
-function animate(element) {
-    var anim = bodymovin.loadAnimation({
-        container: element,
-        renderer: 'svg',
-        loop: false,
-        autoplay: false,
-        path: element.getAttribute('data-json')
-    });
-    element.onclick = function() {
-        anim.goToAndPlay(0);
-    };
-}
+    function animate(element) {
+        var anim = bodymovin.loadAnimation({
+            container: element,
+            renderer: 'svg',
+            loop: false,
+            autoplay: false,
+            path: element.getAttribute('data-json')
+        });
+        console.log(bodymovin.loadAnimation);
+        element.addEventListener("click", function() {
+            anim.goToAndPlay(0);
+        });
+    }
 
-var containers = document.getElementsByClassName('container');
+    var containers = document.getElementsByClassName('container');
 
-for (var i = containers.length - 1; i >= 0; i--) {
-    animate(containers[i]);
-}
+    for (var i = containers.length - 1; i >= 0; i--) {
+        animate(containers[i]);
+        console.log("woot");
+    }
+})
