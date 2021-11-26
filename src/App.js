ae4b5a9b97d92894f4d1bc547bd95e0156e571ee
Stephanie Cree <16628960+skcele@users.noreply.github.com>
2021-11-26 14:38:52 -0600

    added photo, text and moved images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './styles/App.css';
+// import Palm from "./photos/palms.png";
 
 function App() {
   return (
@@ -17,7 +18,19 @@ function App() {
         </nav>
       </header>
       <main>
-        <div className="main-photo"></div>
+        <div className="main-photo">
+          <div className="main-photo_description">
+            <h3>Stephanie K Cree is a UX Engineer by day and storyteller by night.
+            <br/>
+            <br/>
+            I solve problems with design and code.<br/>
+            I tell stories with words and multimedia art.
+            </h3>
+          </div>
+        </div>
+        <div className="main-section">
+          <h3>I am currently a Front-End Developer and Designer at IBM, where I work on prototyping fantastic user experiences, facilitating collaboration, and integrating motion across products.</h3>
+        </div>
       </main>
     </div>
   );
